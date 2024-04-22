3adb8583b2bce49c15b0e97a1d27be4a7600766d
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2024-04-22 23:54:39 +0700

    Feature/spring: Add CRUD Spring App

---
+++ springboot/src/main/java/hody/springboot/SpringbootApplication.java
@@ -0,0 +1,13 @@
+package hody.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
